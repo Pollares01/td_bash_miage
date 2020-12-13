da407c88afd49eb7cba45dc58c5da7098c979ec3
Paul KELBERT <kelbertpaul@gmail.com>
2020-12-13 18:19:08 +0100

    bla'

--- messagerie.sh
+++ messagerie.sh
@@ -14,47 +14,50 @@ for file in $( find /messagerie/messages_$(whoami)/ -type f -ctime +1 ! -name "*
 	rm $file2
 done
 
+for l in $(cat /etc/passwd | grep "/bin/bash" |  awk -F: '{ print $1}'); do
+    listUser+="$l \"\" false "
+    if [ ! -d "/messagerie/messages_$l" ]; then
+        dossier="/messagerie/messages_$l"
+        mkdir $dossier
+    fi
+done
+
 while [ ! "$mode" = "Quitter" ]; do
-    mode=$(dialog --stdout --title "Messagerie" --menu "Que voulez vous faire ?" 20 60 15 "Lire ses messages" "" "Envoyer un nouveau message" "" "Quitter" "")
+    mode=$(dialog --title "Messagerie" --menu "Que voulez vous faire ?" 20 60 15 "Voir ses messages" "" "Envoyer un nouveau message" "" "Quitter" "" 2>&1 >/dev/tty)
     if [ "$mode" = "Envoyer un nouveau message" ]; then
-        destinataires=$(dialog --stdout --title "Messagerie" --checklist "A qui voulez vous envoyer un message ?" 20 60 15 \
-                "paul" "" false "paul2" "" false)
+        destinataires=$(dialog --title "Messagerie" --checklist "A qui voulez vous envoyer un message ?" 20 60 15 \
+                $listUser 2>&1 >/dev/tty)
         if [ "$?" = "1" ]; then
             continue
         fi        
         while [ -z ${destinataires[@]} ]; do
-            dialog --stdout --title "Messagerie" --msgbox "Veuillez entrer au moins un destinataire." 5 60
-            destinataires=$(dialog --stdout --title "Messagerie" --checklist "A qui voulez vous envoyer un message ?" 20 60 15 \
-                "paul" "" false "paul2" "" false)
+            dialog --stdout --title "Messagerie" --msgbox "Veuillez entrer au moins un destinataire." 5 60 2>&1 >/dev/tty
+            destinataires=$(dialog --title "Messagerie" --checklist "A qui voulez vous envoyer un message ?" 20 60 15 \
+                $listUser 2>&1 >/dev/tty)
             if [ "$?" = "1" ]; then
                 continue
             fi       
         done
-        for variable in ${destinataires[@]}; do
-            if [ ! -d "/messagerie/messages_$variable" ]; then
-                dossier="/messagerie/messages_$variable"
-                mkdir $dossier
-            fi
-        done
-        sujet=$(dialog --stdout --title "Messagerie" --inputbox "Quel est le sujet de votre message ?" 20 60) 
+
+        sujet=$(dialog --title "Messagerie" --inputbox "Quel est le sujet de votre message ?" 20 60 2>&1 >/dev/tty) 
         if [ "$?" = "1" ]; then
             continue
         fi  
         while [ -z $sujet ]; do
-            dialog --stdout --title "Messagerie" --msgbox "Veuillez entrer un sujet." 5 60
-            sujet=$(dialog --stdout --title "Messagerie" --inputbox "Quel est le sujet de votre message ?" 20 60)
+            dialog --title "Messagerie" --msgbox "Veuillez entrer un sujet." 5 60 2>&1 >/dev/tty
+            sujet=$(dialog --title "Messagerie" --inputbox "Quel est le sujet de votre message ?" 20 60 2>&1 >/dev/tty)
             if [ "$?" = "1" ]; then
                 continue
             fi  
         done
         sujet=$(echo $sujet | sed -e "s/ /_/g")
-        contenu=$(dialog --stdout --title "Messagerie" --editbox "/messagerie/test" 20 60)
+        contenu=$(dialog --title "Messagerie" --editbox "/messagerie/test" 20 60 2>&1 >/dev/tty)
         if [ "$?" = "1" ]; then
             continue
         fi  
         while [ -z $contenu ]; do
-            dialog --stdout --title "Messagerie" --msgbox "Veuillez entrer un message." 5 60
-            contenu=$(dialog --stdout --title "Messagerie" --editbox "/messagerie/test" 20 60)
+            dialog --title "Messagerie" --msgbox "Veuillez entrer un message." 5 60 2>&1 >/dev/tty
+            contenu=$(dialog --title "Messagerie" --editbox "/messagerie/test" 20 60 2>&1 >/dev/tty)
             if [ "$?" = "1" ]; then
                 continue
             fi  
@@ -63,28 +66,33 @@ while [ ! "$mode" = "Quitter" ]; do
             echo -e "Destinataire(s) : $destinataires\n\nSujet : $sujet\n\nContenu : $contenu" > "/messagerie/messages_$variable/$sujet-_De_:_$(whoami)"
         done
     else
-        if [ "$mode" = "Lire ses messages" ]; then
+        if [ "$mode" = "Voir ses messages" ]; then
             options=""
             cd "/messagerie/messages_$(whoami)/"
             for file in $(ls *); do
                 options+="$file \"\" "
             done
             if [ "$options" != "" ]; then
-                messageALire=$(dialog --stdout --title "Messagerie" --menu "Choissiez un message" 20 60 10 $options)
-                queFaire=$(dialog --stdout --title "Messagerie" --menu "Que voulez vous faire ?" 20 60 15 "Lire le message" "" "Supprimer le message" "")
+                messageALire=$(dialog --title "Messagerie" --menu "Choissiez un message" 20 60 10 $options 2>&1 >/dev/tty)
+                queFaire=$(dialog --title "Messagerie" --menu "Que voulez vous faire ?" 20 60 15 "Lire le message" "" "Supprimer le message" "" 2>&1 >/dev/tty)
+                if [ "$?" = "1" ]; then
+                    continue
+                fi 
                 if [ "$queFaire" = "Lire le message" ]; then
                     if [ ${messageALire#*.} = "tar" ]; then
                         fichier=$(tar -xvf $messageALire)
-                        dialog --stdout --title "Messagerie" --textbox "/messagerie/messages_$(whoami)/$fichier" 20 60
+                        dialog --title "Messagerie" --textbox "/messagerie/messages_$(whoami)/$fichier" 20 60 2>&1 >/dev/tty
                         rm $fichier
                     else 
-                        dialog --stdout --title "Messagerie" --textbox "/messagerie/messages_$(whoami)/$messageALire" 20 60
+                        dialog --title "Messagerie" --textbox "/messagerie/messages_$(whoami)/$messageALire" 20 60 2>&1 >/dev/tty
                     fi  
                 else
                     if [ "$queFaire" = "Supprimer le message" ]; then
                         rm "/messagerie/messages_$(whoami)/$messageALire"
                     fi
                 fi
+            else
+                dialog --title "Messagerie" --msgbox "Vous n'avez aucun message." 5 60 2>&1 >/dev/tty
             fi
             clear
         else
